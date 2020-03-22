2fa18a94e550c2114c88b883cbd54dfc56937797
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2020-03-22 19:16:53 +0100

    a starting point for a landing page

--- src/router.js
+++ src/router.js
@@ -1,17 +1,28 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ShopRegistration from './components/ShopRegistration.vue';
+import Home from './components/Landing.vue';
 
 Vue.use(Router);
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    name: 'Home',
+    props: {
+      msg: 'Wahts happeing yo?!'
+    }
+   },
+  {
+    path: '/register/shop',
+    name: 'ShopRegistration',
+    component: ShopRegistration,
+  }
+]
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/register/shop',
-      name: 'ShopRegistration',
-      component: ShopRegistration,
-    }
-  ],
+  routes: routes
 });
